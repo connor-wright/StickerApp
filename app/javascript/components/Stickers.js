d89dc8eb59ea2c3c51b7c8df155d5f281f586c30
Connor Wright <connor.d.wright1996@gmail.com>
2020-09-04 02:25:11 +0000

    Added new outline for component

---
+++ app/javascript/components/Stickers.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Stickers extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Stickers
