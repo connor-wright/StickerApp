b253d162dc47dd11a39b9325211ef5f0ad2b6133
Connor Wright <connor.d.wright1996@gmail.com>
2020-11-07 18:33:07 +0000

    Added wrapper for handling imgur request

--- app/controllers/v1/imgur_api_wrapper_controller.rb
+++ app/controllers/v1/imgur_api_wrapper_controller.rb
@@ -1,28 +1,20 @@
 class V1::ImgurApiWrapperController < ApplicationController
+  include V1::ImgurApiWrapperHelper
   protect_from_forgery with: :null_session
   
   def index
-    
     imgur_client = ImgurApiWrapper::Client.new()
     
     if(id = params[:img_id])
-      render :json => imgur_client.image(id)
+      handleRequest(imgur_client.image(id), 'Could not get image')
     else
-      render :json => imgur_client.search_image("nature")
+      handleRequest(imgur_client.search_image("nature"), 'Could not get image')
     end
   end
   
   def search
     imgur_client = ImgurApiWrapper::Client.new()
     query = params.require(:query)
-    response = imgur_client.search_image(query)
-    if(response.status == 200)
-      render :json => response.body
-    else 
-      render json: 
-      {
-        :error => 'Imgur returned an error'
-      }.to_json, :status => response.status, :error => "internal-server-error"
-    end
+    handleRequest(imgur_client.search_image(query), 'Imgur returned an error')
   end
 end
