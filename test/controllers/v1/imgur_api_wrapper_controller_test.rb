9c76caf4dbea1089860578de41f9b9a0efa4216c
Connor Wright <connor.d.wright1996@gmail.com>
2020-11-10 01:47:52 +0000

    Added todo comment

--- test/controllers/v1/imgur_api_wrapper_controller_test.rb
+++ test/controllers/v1/imgur_api_wrapper_controller_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-
+#TODO test requires imgur being up need to mock this connectin
 class V1::ImgurApiWrapperControllerTest < ActionDispatch::IntegrationTest
   test "imgur api wrapper should return valid json" do
     get '/v1/imgur_api'
